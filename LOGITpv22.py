b204b58eeb1bae79b058fb5f02722015f5c22a5f
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2022-12-08 10:02:21 +0200

    n

--- LOGITpv22.py
+++ LOGITpv22.py
@@ -1,4 +1,58 @@
+from math import *
+#1
+print("Puu läbimõõdu arvutamine")
+C=float(input("Puu ümbermõõt: "))
+d=2*(C/(2*pi))
+print(f"Vastus:\nPuu läbimõõduga {C} ümbermõõt võrdub {d}")
+
+#2
+print("Ristkülikukujulise maatüki diagonaal")
+N=float(input("Sisesta ristküliku 1. külje pikkus => "))
+M=float(input("Sisesta ristküliku 2. külje pikkus => "))
+d=sqrt(N**2+M**2)
+print(f"Maatüki diagonaal on {d} m**2")
+
+#3
+
+
+
+
+
+#--------------------------------
+ 
+print()
+print("Ruudu karakteristikud")
+a=float(input("Sisesta ruudu külje pikkus => "))
+S=a**2
+print("Ruudu pindala", round(S,1))
+P=4*a
+print("Ruudu ümbermõõt", round(P,1))
+di=a*sqrt(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+#-----------------------------
+print("Ristküliku karakteristikud")
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))
+S=b*c
+print("Ristküliku pindala", round(S,1))
+P=2*(b+c)
+print("Ristküliku ümbermõõt", round(P,1))
+di=sqrt(b*2+c*2)
+print("Ristküliku diagonaal", round(di,1))
+print()
+print("Ringi karakteristikud")
+r=float(input("Sisesta ringi raadiusi pikkus => "))
+d=2*r
+print("Ringi läbimõõt", round(d,1))
+S=pi*r**2
+print("Ringi pindala", round(S,2))
+C=2*pi*r
+print("Ringjoone pikkus", round(C,2))
+
+#----------------------------------------------
 from math import * #*-kõik funktsioonid on vaja kasutada. kasutame: sin()
+
 #import math kasutame: math.sin()
 #2.osa Math kasutamine 
 print("Ristküliku ja ringi pindalad")
