757b95bbb64e60a048877b308f0a5745c53dca0c
marinaoleinik <marina.oleinik@tthk.ee>
2022-12-21 14:58:30 +0200

    TypeError

--- LOGITpv22.py
+++ LOGITpv22.py
@@ -1,6 +1,50 @@
 from math import *
 from random import *
 #21/12/22
+#3
+while True:
+    try:
+        p=float(input("Pikkus: "))
+        if p>0: break
+    except:
+        print("On vaja numbreid kirjutada, mis on suurem kui 0")
+while True:
+    try:
+        l=float(input("Laius: "))
+        if l>0: break
+    except:
+        print("On vaja numbreid kirjutada, mis on suurem kui 0")
+while True:
+    v=input("Kas tahad remonti teha? ")
+    if v.upper()=="JAH" or v.upper()=="EI" : break
+if v.upper()=="JAH":
+    while True:
+        try:
+            hind=float(input("Kui palju maksam m^2"))
+            break
+        except TypeError:
+            print()
+    hind=l*p*hind
+    print(f"Remonti hind on {hind}")
+else:
+    pass
+
+
+
+#2
+while True:
+    nimi1=input("1. Mis on sinu nimi? ")
+    if nimi1.isalpha(): break
+while True:
+    nimi2=input("2. Mis on sinu nimi? ")
+    if nimi2.isalpha(): break
+if nimi1=="Anna" and nimi2=="Inna": print("Neid on täna pinginaabrid")
+
+
+
+
+
+#1
 while True:
     nimi=input("Mis on sinu nimi? ")
     if nimi.isalpha(): break
