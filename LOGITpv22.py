cf92cda7468e6fe1e07382b40db3113cfadffb19
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2023-02-06 12:25:55 +0200

    Isikukoodid ja arvud

--- LOGITpv22.py
+++ LOGITpv22.py
@@ -1,17 +1,22 @@
 from math import *
 from random import *
 #3/03/23
+arvud=[]
+isikukoodid=[]
 
 while True:
     ik=input("Anna isikukood: ") #str
+    if ik=="1": break
     if len(ik)!=11:
         print("Liiga palju või liiga vähe sümboleid. Sisesta veel kord:")
+        arvud.append(ik)
     else:
         print("Isikukoodi kontroll")
         ik_list=list(ik)
         s1=int(ik_list[0]) #"1"->1
         if s1 not in [1,2,3,4,5,6]:
             print("Esimene sümbol ei ole õige!")
+            arvud.append(ik)
         else:
             print("Esimene sümbol on õige")
             y=ik_list[1]+ik_list[2] #aasta
@@ -19,6 +24,7 @@ while True:
             d=ik_list[5]+ik_list[6] #päev
             if (int(m)<1 or int(m)>13) and (int(d)<1 or int(d)>31):
                 print("Sünnipäev ei saa luua")
+                arvud.append(ik)
             else:
                 if s1==1 or s1==2:
                     yy="18"
@@ -29,11 +35,28 @@ while True:
                 spaev=d+"."+m+"."+yy+y #ei ole 18..,19..,20..
                 print(f"Sünnipäev on {spaev}")
                 print(f"Viimane number: {ik_list[-1]}")
+                kontrollnr=0
+                
+                hhh=int(ik_list[8]+ik_list[9]+ik_list[10])
+                if 1<=hhh<=10:
+                    haigla="kuresaare Haigla"
+                elif 11<=hhh<=19:
+                    haigla="Tartu Ülikooli Naistekliinik, Tartumaa, Tartu"
+                elif 21<=hhh<=220:
+                    haigla="Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla"
+                else:
+                    haigla=" ---"
 
-
-
-
-
+                if int(ik_list[0])%2==0:
+                    sugu="naine"
+                else:
+                    sugu="mees"
+                print(f"See in {sugu}, sünnipäev{spaev}. Ta on sündinud {haigla}")
+                isikukoodid.append(ik)
+print(isikukoodid)
+arvud=list(map(int,arvud))
+arvud.sort()
+print(arvud)
 
 #31/01/23
 #3
